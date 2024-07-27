100825bf69ba518e98759eb13cbce66e5fb0ae9f
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-27 23:52:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ function App() {
   const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
 
   useEffect(() => {
-    // Example weather data
-    const weatherInput = { temperature: 25, conditions: "Sunny" };
+    // Example weather data:
+    const weatherInput = { temperature: 25, conditions: "Sunny",temparature:20,conditions:"Cloud" };
     setWeather(weatherInput);
   }, []);
 
