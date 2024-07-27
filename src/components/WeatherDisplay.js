90386d672d94797234ea791e0fc2cfa7785710e6
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-27 22:12:02 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,6 +4,9 @@ function WeatherDisplay({ weather }) {
   const { temperature, conditions } = weather;
   const color = temperature > 20 ? 'red' : 'blue';
 
+  console.log("Temperature: ", temperature);
+  console.log("Color: ", color);
+
   return (
     <div>
       <p style={{ color }}>Temperature: {temperature}</p>
