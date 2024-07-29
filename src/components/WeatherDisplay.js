c3d7e9fccc4fed81038bf82e2000736303f62498
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-29 20:18:05 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function WeatherDisplay({ weather }) {
-  const { Temperature, Conditions } = weather;
-  const color = Temperature > 20 ? 'rgb(255, 0, 0)' : 'blue'; // Conditional color based on temperature
+  const { temperature, conditions } = weather;
+  const color = temperature > 20 ? 'rgb(255, 0, 0)' : 'blue'; // Conditional color based on temperature
 
   return (
     <div>
-      <p style={{ color }}>Temperature: {Temperature}</p>
-      <span style={{ color }}>Conditions: {Conditions}</span>
+      <p style={{ color }}>Temperature: {temperature}</p>
+      <span style={{ color }}>Conditions: {conditions}</span>
     </div>
   );
 }
